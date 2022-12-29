0e14235d48f870f9a1571cffc8e3970a21b3bcbd
Jason D'Oyley <jdfdoyley@gmail.com>
2022-12-29 12:39:19 -0500

    Create method to compare two double

--- DecimalComparator/src/App.java
+++ DecimalComparator/src/App.java
@@ -6,6 +6,30 @@
 
 public class App {
   public static void main(String[] args) throws Exception {
-    System.out.println("Hello, World!");
+    System.out.println(areEqualByThreeDecimalPlaces(-3.1756, -3.175)); // true
+    System.out.println(areEqualByThreeDecimalPlaces(3.175, 3.1765)); // false
+    System.out.println(areEqualByThreeDecimalPlaces(3.0, 3.0)); // true
+    System.out.println(areEqualByThreeDecimalPlaces(-3.123, 3.123)); // false
+  }
+
+  /**
+   * @param a
+   * @param b
+   * @return boolean
+   */
+  public static boolean areEqualByThreeDecimalPlaces(double a, double b) {
+    int temp;
+
+    temp = (int) (a * 1000);
+    a = (double) temp / 1000;
+
+    temp = (int) (b * 1000);
+    b = (double) temp / 1000;
+
+    if (a == b) {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
