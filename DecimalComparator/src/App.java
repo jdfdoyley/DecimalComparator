b86094edec6cc3f1bcb00aacc1c5cea00679adce
Jason D'Oyley <jdfdoyley@gmail.com>
2022-12-29 12:19:01 -0500

    Add documentation to readme file

--- DecimalComparator/src/App.java
+++ DecimalComparator/src/App.java
@@ -1,5 +1,11 @@
+/**
+ * Author: Jason D. F. D'Oyley
+ * Date: Dec 29, 2022
+ * Exercise Name: DecimalComparator
+ */
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+  public static void main(String[] args) throws Exception {
+    System.out.println("Hello, World!");
+  }
 }
